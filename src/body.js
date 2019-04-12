c9af5c7dcee61c03bfe43fa6c08aa3ecae1b3e6c
Theodore Boudros <teamboud@gmail.com>
2019-04-12 14:29:41 +0300

    Turning it into a module

---
+++ src/body.js
@@ -0,0 +1,18 @@
+
+
+function createElement(){
+    
+}
+
+function returnElement(obj, cb){
+    var begin = "";
+    var end = "";
+    setTag(obj["type"], (tag_begin, tag_end)=>{
+        
+    })
+    cb(begin, end)
+}
+
+function setTag(type, cb){
+    
+}
--- translate.js
+++ translate.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const element = require('./src/element_old');
 
 var args = process.argv.splice(process.execArgv.length + 2);
 fs.readFile(args[0], function(err, file){
@@ -10,7 +11,7 @@ fs.readFile(args[0], function(err, file){
 function createHtml(head, body){
     createHead(head, function(headstr){
         var html = "<head>"+headstr+"</head>";
-        createElement(body, function(str){
+        element(body, function(str){
             html = html + str
         });
         fs.writeFile(args[0].split('.')[0] + ".html", html, function(err){if (err) throw err;});
@@ -26,73 +27,4 @@ function createHead(head, cb){
         text = text + '<link href="'+href+'" rel="stylesheet" type="text/css">'
     })
     cb(text);
-}
-
-function createElement(element, cb){
-    returnElement(element, function(beggining, end){
-        var text = "";
-        var i = 0
-            if(element["children"] !== undefined){
-                text = beggining
-                var i = 0;
-                element["children"].forEach(child => {
-                    i++;
-                    createElement(child, function(str){
-                        text = text + str
-                    })
-                    if(i==element["children"].length){
-                        text = text + end
-                        cb(text)
-                    }
-                })  
-            }else{
-                text = beggining + end;
-                cb(text);
-            }
-    })
-}
-
-function returnElement(element, cb){
-    var beggining = "", end = ""
-    if(element["type"] == "button"){
-        beggining = '<input type="submit" value="'+element["value"]+'"'
-        end = ""
-    }else if(element["type"] == "a"){
-        beggining = "<a href='" + element['href'] + "'"
-        end = "</a>"
-    }else if(element["type"] == "body"){
-        beggining = "<body"
-        end = "</body>"
-    }else if(element["type"] == "div"){
-        beggining = '<div id="'+element["id"]+'"'
-        end = "</div>"
-    }       
-    if(element["class"] !== undefined){
-        beggining = beggining + ' class="'
-        var i = 0;
-        element['class'].forEach(cls => {
-            i++;
-            beggining = beggining + cls + " "
-            if(i === element['class'].length){
-                beggining = beggining.slice(0, -1) + '"'
-            }
-        })
-    }
-    if(element["style"] !== undefined){
-        beggining = beggining + ' style="'
-        var length_keys = Object.keys(element["style"]).length
-        var i = 0;
-        for (var style in element["style"]){
-            i++
-            beggining = beggining + style + ': ' + element["style"][style] + "; "
-            if(i == length_keys){
-                beggining = beggining.slice(0, -1) + '"'
-            }
-        }
-    }
-    beggining = beggining + ">"
-    if(element["html"] !== undefined){
-        beggining = beggining + element['html']    
-    }
-    cb(beggining, end)
 }
