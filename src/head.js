dcdf6b36d715a4eaedaebeddf0e63dfaac88968c
Theodore Boudros <teamboud@gmail.com>
2019-04-12 16:16:41 +0300

    Fixed head style bug

--- src/head.js
+++ src/head.js
@@ -3,9 +3,12 @@ module.exports = (head, cb)=>{
     if(head["title"] !== undefined){
         text = text + '<title>' + head["title"] + '</title>'
     }
-    head["css"].forEach(href =>{
-        text = text + '<link href="'+href+'" rel="stylesheet" type="text/css">'
-    })
+
+    if(head["css"] !== undefined){
+        head["css"].forEach(href =>{
+            text = text + '<link href="'+href+'" rel="stylesheet" type="text/css">'
+        })
+    }
     var html = "<head>"+text+"</head>";
     cb(html);
 }
